2b033fcf67f2f4552cd9f593c8d38bd9099792c2
nc <scorpioncarr@gmail.com>
2018-12-10 10:19:10 -0600

    Testing contact us page

--- send_form_email.php
+++ send_form_email.php
@@ -2,21 +2,21 @@
 if(isset($_POST['email']))
 {
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "nc@settle.cc";
+    $email_to = "sa@settle.cc";
     $email_subject = "e-mail from website";
 
     function died($error)
     {
         // your error code can go here
         echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-        echo "These errors appear below.<br /><br />";
-        echo $error."<br /><br />";
-        echo "Please go back and fix these errors.<br /><br />";
+        echo "These errors appear below . <br /><br />";
+        echo $error . "<br /><br />";
+        echo "Please go back and fix these errors . <br /><br />";
         die();
     }
 
     // validation expected data exists
-    if(!isset($_POST['name']) ||
+    if(!isset($_POST['full_name']) ||
         !isset($_POST['email']) ||
         !isset($_POST['telephone']) ||
         !isset($_POST['comments']))
@@ -24,7 +24,7 @@ if(isset($_POST['email']))
             died('We are sorry, but there appears to be a problem with the form you submitted.');
         }
 
-    $name = $_POST['name']; // required
+    $name = $_POST['full_name']; // required
     $email_from = $_POST['email']; // required
     $telephone = $_POST['telephone']; // not required
     $comments = $_POST['comments']; // required
@@ -71,7 +71,7 @@ if(isset($_POST['email']))
 
     #include your own success html here -->
 
-    print "Thank you for contacting us. We will be in touch with you very soon.";
+    #print "Thank you for contacting us. We will be in touch with you very soon.";
 
 
 
